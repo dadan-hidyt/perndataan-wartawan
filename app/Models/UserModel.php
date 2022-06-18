a9b88b4b0109d6694c9a3fca8350e7004fbdad74
dadan-hidyt <dadanhidyt@gmail.com>
2022-06-18 12:22:54 +0700

    model

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class UserModel extends Model
+class UserModel extends Model implements Authenticatable
 {
+    use \Illuminate\Auth\Authenticatable;
     use HasFactory;
     public $table = 'tb_admin';
     public $primaryKey = "user_id";
@@ -15,4 +16,5 @@ class UserModel extends Model
       'username',
       'password'
     ];
+
 }
